bc82f8bf997b22487bfb8186b83279567e2a3815
Abdelhalimoumellouk-cmyk <abdelhalimoumellouk@gmail.com>
2026-02-12 14:10:38 +0100

    suppression dossier Simulation + evaporation pheromones(return?) + modification HTML + modifications CSS

---
+++ evap.py
@@ -0,0 +1,26 @@
+from world import espace
+from draw import draw
+#import time
+
+
+def evaporation(world, taux):  
+    for y in range(len(world)):
+        for x in range(len(world[y])):
+
+            if type(world[y][x]) == int and world[y][x] > 0:
+                
+                
+                world[y][x] = int(world[y][x] * (1 - taux))
+
+               
+                if world[y][x] < 1:
+                    world[y][x] = 1
+                    #return
+
+
+
+
+    #time.sleep(0.1)
+
+evaporation(espace, 0.05)   # 5% d'évaporation par tick
+draw(espace, [])
